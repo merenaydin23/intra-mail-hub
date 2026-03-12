ffaa92a2446864fc2745e7b369a9bf0e218c177a
Muhammed Eren Aydın <muhammedeerenaydin7@gmail.com>
2026-03-12 16:15:13 +0300

    feat: setup Firebase configuration keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase yapılandırma ayarlarınız - Firebase Console'dan alacağınız bilgilerle değiştirin
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "SENIN_API_KEY",
-  authDomain: "projen.firebaseapp.com",
-  projectId: "projen",
-  storageBucket: "projen.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyDR28h-ns4E70SN8QXw5iuCyEjJcFNv0Is",
+  authDomain: "intra-mail-hub.firebaseapp.com",
+  projectId: "intra-mail-hub",
+  storageBucket: "intra-mail-hub.firebasestorage.app",
+  messagingSenderId: "1082646812541",
+  appId: "1:1082646812541:web:99d7fd4d0adb8a18281e14",
+  measurementId: "G-QH2SG4N9D6"
 };
 
 // Initialize Firebase
